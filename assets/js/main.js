1b27d2493e52a799b853d33bd5387f30715a892a
Nguyễn Văn Thanh <vanthanh23799@gmail.com>
2021-08-03 14:02:17 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(()=>{
+    $('#menu-button').click(()=>{
+        $('.menu-nav').first().slideToggle("slow");
+    });
+});
+
+$(document).ready(function(){
+  var data = {
+    dots: true,
+    infinite: true,
+    arrows: false,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    variableWidth: true,
+    initialSlide:1
+  };
+  $(".blog-slider").slick(data);
+});
